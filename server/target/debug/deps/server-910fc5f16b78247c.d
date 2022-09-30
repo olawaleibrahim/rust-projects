c9e01c313d6b3bb1ed2795b64aae2cb68ab94ad4
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-09-30 22:02:30 +0100

    initial commit

---
+++ server/target/debug/deps/server-910fc5f16b78247c.d
@@ -0,0 +1,5 @@
+/home/olawale/Desktop/PROJECTS/rust-projects/server/target/debug/deps/server-910fc5f16b78247c.rmeta: src/main.rs
+
+/home/olawale/Desktop/PROJECTS/rust-projects/server/target/debug/deps/server-910fc5f16b78247c.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/deps/server-f1f3173b0ff0fb94.d
@@ -0,0 +1,5 @@
+/home/olawale/Desktop/PROJECTS/rust-projects/server/target/debug/deps/server-f1f3173b0ff0fb94.rmeta: src/main.rs
+
+/home/olawale/Desktop/PROJECTS/rust-projects/server/target/debug/deps/server-f1f3173b0ff0fb94.d: src/main.rs
+
+src/main.rs:
