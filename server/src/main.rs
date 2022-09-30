3fbdbc695d256f9c0d4429d1570af8af6f7e661c
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-09-30 23:12:15 +0100

    writing a response

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,10 +7,9 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-    println!(
-        "Request {}:",
-        String::from_utf8_lossy(&buffer[..])
-    )
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush();
 
 }
 
