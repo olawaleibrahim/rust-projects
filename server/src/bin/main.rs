6b287ad8bfc63aba3bcd807eca656e318c016f9f
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-11-07 14:47:28 +0100

    multi threaded functionality complete

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -5,6 +5,8 @@ use std::time::Duration;
 use std::thread;
 use std::fs;
 
+use server::ThreadPool;
+
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
@@ -44,10 +46,12 @@ fn main() {
     let listener = TcpListener::bind(
         "127.0.0.1:7878").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
         
